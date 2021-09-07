8abb1e36d37356c4112f84007d7ebf861b56db09
Gabriel Calderón <garry019@gmail.com>
2021-09-07 15:23:57 -0500

    Deploying Files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-    
+        <h1>Profile</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Router from './Router';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+      <App />,
   document.getElementById('root')
 );
