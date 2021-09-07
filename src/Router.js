ccc8933f97e75442fd64e5eed72337597d3cdbc0
Gabriel Calderón <garry019@gmail.com>
2021-09-07 13:56:03 -0500

    Cleaning Files

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import PageNotFound from './Error';
 import Profile from './components/profile/Profile'
 
 const Router = () => {
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Switch>
                 <Route exact path="/" component={Profile} />
                 <Route component={PageNotFound} />
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
